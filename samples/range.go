34f7b99334ccfbbe381bd36c9966d989bd0f3a5d
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-10-02 10:15:29 +0300

    test break in range

--- samples/range.go
+++ samples/range.go
@@ -8,3 +8,20 @@ func simpleRange() {
 	}
 	println(x)
 }
+
+func breakRange() {
+	x := [5]int{10, 20, 30, 40, 50}
+	for i, v := range x {
+		if v == 40 {
+			continue
+		}
+		for j, u := range x {
+			println(i, j)
+			if u > 30 {
+				break
+			}
+		}
+		println("end of cycle")
+	}
+	println(x)
+}
