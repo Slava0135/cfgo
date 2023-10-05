d431bcd149e83e7731b07d126b3ccf793dd9b5a3
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-10-05 14:16:22 +0300

    nested continue sample update

--- samples/continue.go
+++ samples/continue.go
@@ -3,10 +3,11 @@ package samples
 func nestedContinue() {
 	var ever = true
 	for i := 0; ever; i += 1 {
-		for true {
-			if false {
+		for ever {
+			if i > 10 {
 				break
 			}
+			print("nested loop")
 		}
 		print("loop body")
 		if 42 != 42 {
